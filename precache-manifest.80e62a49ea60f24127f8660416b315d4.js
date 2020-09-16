2ae22d3bf7c210dfc66186818dac9c104b360af6
novacane <mudakmudaka@gmail.com>
2020-09-16 18:31:28 -0500

    Updates

---
+++ precache-manifest.80e62a49ea60f24127f8660416b315d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950f0f9aebe8d6d9edc031b8da2b2070",
+    "url": "/lux-academy/index.html"
+  },
+  {
+    "revision": "16cbb03bb3c21dc58ee5",
+    "url": "/lux-academy/static/css/main.1a7a4410.chunk.css"
+  },
+  {
+    "revision": "45803b243a5e386d80ce",
+    "url": "/lux-academy/static/js/2.cf50dbf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lux-academy/static/js/2.cf50dbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cbb03bb3c21dc58ee5",
+    "url": "/lux-academy/static/js/main.31c3444a.chunk.js"
+  },
+  {
+    "revision": "a796803cfad9c1f2b138",
+    "url": "/lux-academy/static/js/runtime-main.f2fdcb4f.js"
+  }
+]);
