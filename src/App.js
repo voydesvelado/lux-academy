7e550a5fa3c156da3d7d1622bc500ab0661ea136
novacane <mudakmudaka@gmail.com>
2020-09-16 18:39:25 -0500

    correccion inicio

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact component={Inicio} />
         <Route path="/galeria" exact component={Galeria} />
       </Switch>
+      <Inicio />
     </Router>
   );
 }
